c3ee38283afc3ce60a517df68943b6e817128e49
Theo <13920881+theloni-monk@users.noreply.github.com>
2020-03-14 23:04:46 -0700

    wrote vector class

--- 8ball/src/util/pVector.java
+++ 8ball/src/util/pVector.java
@@ -1,25 +1,37 @@
-//WRITEME: pVector methods
 package util;
 public class pVector{
     public double x;
     public double y;
 
-    public static pVector add(pVector p1, pVector p2){
+    public pVector(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
 
+    public pVector(double r, double theta, boolean polar){
+        this.x = r * Math.cos(theta);
+        this.y = r * Math.sin(theta);
+    }
+
+    // cartesian operations:
+    public static pVector add(pVector p1, pVector p2){
+        return new pVector(p1.x + p2.x, p1.y + p2.y);
     }
     public static pVector sub(pVector p1, pVector p2){
-
+        return new pVector(p1.x - p2.x, p1.y - p2.y);
     }
-    public static pVector mult(pVector p1, pVector p2){
-
+    public static pVector mult(pVector p1, double s){
+        return new pVector(p1.x * s, p1.y * s);
     }
-    public static pVector dot(pVector p1, pVector p2){
-
+    public static double dot(pVector p1, pVector p2){
+        return (p1.x*p2.x) + (p1.y*p2.y);
     }
+
+    // polar functions
     public double mag(){
-
+        return Math.sqrt((this.x*this.x)+(this.y*this.y));
     }
     public double heading(){
-
+        return Math.atan(this.y/this.x);
     }
 }
