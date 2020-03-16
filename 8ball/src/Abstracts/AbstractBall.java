0c74719039e0032bda1c89edab7f9e324536b16f
Theo <13920881+theloni-monk@users.noreply.github.com>
2020-03-15 18:51:35 -0700

    changed visibility of fields and added necessary fields

--- 8ball/src/Abstracts/AbstractBall.java
+++ 8ball/src/Abstracts/AbstractBall.java
@@ -2,7 +2,21 @@ package Abstracts;
 import util.pVector;
 //abstract classes used to share code between server and client
 abstract public class AbstractBall{
-    protected pVector position;
-    protected pVector velocity;
-    final double radius = 6; //cm ~ 2.375 inches (standard billiard ball size)
+    public pVector position;
+    public pVector velocity;
+    public boolean rest;
+    public final double radius = 6; //cm ~ 2.375 inches (standard billiard ball size)
+    public final double mass = 0.6; //TODO: tune mass
+    protected int number;
+    protected boolean striped; // for rendering
+
+    public void step(){}
+    /**
+     * @param velocity the velocity to set
+     */
+    public void setVelocity(pVector velocity) {
+        this.velocity = velocity;
+        this.rest = false;
+    }
+
 }
--- 8ball/src/Abstracts/AbstractTable.java
+++ 8ball/src/Abstracts/AbstractTable.java
@@ -2,9 +2,13 @@ package Abstracts;
 import java.util.ArrayList;
 //abstract classes used to share code between server and client
 abstract public class AbstractTable{
-    ArrayList<AbstractBall> balls;
+    protected ArrayList<AbstractBall> balls;
+    protected double friction;
+
+    protected boolean rest; // for when all the balls have 0 velocity
 
     // stanard billiard table dimensions
     final double length = 304.8; //cm ~ 10ft 
     final double width = 152.4; //cm ~ 5ft 
+    // NOTE: 0,0 in the coordinate space of the table will be at the center of the table
 }
