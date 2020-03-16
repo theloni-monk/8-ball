75043f33b69a16218bf2ea4ccd13784bb49626cb
Theo <13920881+theloni-monk@users.noreply.github.com>
2020-03-15 18:53:07 -0700

    added utility class to translate between coordinate spaces

--- 8ball/src/util/CoordTranslator.java
+++ 8ball/src/util/CoordTranslator.java
@@ -7,12 +7,30 @@ public class CoordTranslator{
     public static double[] xRange;
     public static double[] yRange;
 
-    //WRITEME Conversions
-    public static pVector Convert_PixToPos(){
-
+    public CoordTranslator(int[] screenDim,double[] xR,double[] yR){
+        screenWidth = screenDim[0];
+        screenHeight = screenDim[1];
+        xRange = xR;
+        yRange = yR;
+    }
+    
+    public static pVector Convert_PixToPos(int x, int y){
+        // takes in coords and outputs the corrisponding vals from 2,-2
+        double scaleh = (yRange[1] - yRange[0]) / screenHeight;
+        // what one px corresponds to
+        double scalew = (xRange[1] - xRange[0]) / screenWidth;
+        double _x = xRange[0] + (x * scalew);
+        double _y = yRange[1] - (y * scaleh);
+        // because computer coords have 0,0 in the top left
+        return new pVector(_x,_y);
     }
 
-    public static int[] Convert_PosToPix(){
-
+    public static int[] Convert_PosToPix(pVector pos){
+        double scaleh = (yRange[1] - yRange[0]) / screenHeight;
+        //what one px corresponds to
+        double scalew = (xRange[1] - xRange[0]) / screenWidth;
+        int c_x = (int) ((pos.x - xRange[0]) / scalew);
+        int c_y = (int) ((-1 * (pos.y - xRange[1])) / scaleh);
+        return new int[]{c_x, c_y};
     }
 }
